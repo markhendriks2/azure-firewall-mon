b3b08e5a608728bf6704c1b349bde9460f7cc26a
Mark Hendriks <148541586+markhendriks2@users.noreply.github.com>
2024-01-22 16:32:59 +0100

    Update environment.prod.ts

--- firewall-mon-app/src/environments/environment.prod.ts
+++ firewall-mon-app/src/environments/environment.prod.ts
@@ -3,7 +3,5 @@ export const environment = {
   ApplicationInsightsConnectionString: "__APPLICATION_INSIGHTS_CONNECTION_STRING__",
   BuildDate: "__BUILDDATE__",
   AzureMapsSASKey: "__AZURE_MAPS_SAS_KEY__",
-  EventsQueueLength: 100000,
-  eventHubConnectionString: "Endpoint=sb://azurefirewallmonitor01.servicebus.windows.net/;SharedAccessKeyName=AzMonitorListener;SharedAccessKey=VfmdKclz0li59bc+vElsYtfN9IpLjTJWB+AEhIkz35A=;EntityPath=azmonitorcapture",
-  eventHubConsumerGroup: "$Default"
+  EventsQueueLength: 100000
 };
